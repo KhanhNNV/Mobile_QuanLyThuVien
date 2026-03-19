61421d905f5b6fa7a357d289a36adc1a103ec0c4
KhanhNNV <khanhnnv230905@gmail.com>
2026-03-19 15:09:51 +0700

    test

---
+++ app/src/main/java/com/example/quanlythuvien/ui/readers/ReaderListFragment.kt
@@ -0,0 +1,13 @@
+package com.example.quanlythuvien.ui.readers
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.quanlythuvien.R
+
+
+class ReaderListFragment : Fragment(R.layout.fragment_reader_list) {
+
+}
