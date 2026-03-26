050c3c6b4a39a84514373a8dcaa9bae36393d329
Binh <dovanbinh1512@gmail.com>
2026-03-26 11:42:15 +0700

    bo sung

--- app/src/main/java/com/example/quanlythuvien/ui/books/BookImportFragment.kt
+++ app/src/main/java/com/example/quanlythuvien/ui/books/BookImportFragment.kt
@@ -9,4 +9,13 @@ import com.example.quanlythuvien.R
 
 class BookImportFragment : Fragment() {
 
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Nạp giao diện từ file fragment_book_import.xml
+        return inflater.inflate(R.layout.fragment_book_import, container, false)
+    }
+
 }
