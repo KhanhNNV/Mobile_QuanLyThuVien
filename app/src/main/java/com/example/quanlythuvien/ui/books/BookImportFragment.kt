e5a15489410485fe22e91bcc5581b78c5ee5fd1f
Binh <dovanbinh1512@gmail.com>
2026-04-07 15:51:23 +0700

    validate thêm sách và thể loại

--- app/src/main/java/com/example/quanlythuvien/ui/books/BookImportFragment.kt
+++ app/src/main/java/com/example/quanlythuvien/ui/books/BookImportFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AutoCompleteTextView
+import android.widget.Toast
 import com.example.quanlythuvien.R
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 
 class BookImportFragment : Fragment() {
 
@@ -22,10 +26,45 @@ class BookImportFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
 
-        val btnCancel = view.findViewById<com.google.android.material.button.MaterialButton>(R.id.btnCancel)
+        val btnCancel = view.findViewById<MaterialButton>(R.id.btnCancel)
+        val edtBookName = view.findViewById<TextInputEditText>(R.id.edtBookName)
+        val edtAuthor = view.findViewById<TextInputEditText>(R.id.edtAuthor)
+        val edtIsbn = view.findViewById<TextInputEditText>(R.id.edtIsbn)
+        val spinnerCategory =view.findViewById<AutoCompleteTextView>(R.id.spinnerCategory)
+        val edtCost = view.findViewById<TextInputEditText>(R.id.edtCost)
+        val btnSave = view.findViewById<MaterialButton>(R.id.btnSave)
 
         btnCancel.setOnClickListener {
             findNavController().popBackStack()
+        }
+        btnSave.setOnClickListener {
+            val bookName = edtBookName.text.toString().trim()
+            val author = edtAuthor.text.toString().trim()
+            val isbn = edtIsbn.text.toString().trim()
+            val spCategory = spinnerCategory.text.toString().trim()
+            val cost = edtCost.text.toString().trim()
+
+            if (bookName.isEmpty())
+            {
+                edtBookName.error = "Vui lòng nhập tên sách!"
+                edtBookName.requestFocus()
+            }else if(author.isEmpty())
+            {
+                edtAuthor.error = "Vui lòng nhập tên tác giả!"
+                edtAuthor.requestFocus()
+            }else if (isbn.isEmpty()) {
+                edtIsbn.error = "Vui lòng nhập mã ISBN!"
+                edtIsbn.requestFocus()
+
+            } else if (cost.isEmpty() || cost == "VND")
+            {
+                edtCost.error = "Vui lòng nhập giá gốc của sách!"
+                edtCost.requestFocus()
+
+            } else {
+                Toast.makeText(requireContext(), "Đã lưu sách: $bookName", Toast.LENGTH_SHORT).show()
+                findNavController().popBackStack()
+            }
         }
 
     }
--- app/src/main/java/com/example/quanlythuvien/ui/category/CategoryAddFragment.kt
+++ app/src/main/java/com/example/quanlythuvien/ui/category/CategoryAddFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.quanlythuvien.R
 import androidx.navigation.fragment.findNavController
-
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 class CategoryAddFragment : Fragment() {
 
     override fun onCreateView(
@@ -20,11 +22,25 @@ class CategoryAddFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val btnCancelCategory = view.findViewById<com.google.android.material.button.MaterialButton>(R.id.btnCancelCategory)
+        val btnCancelCategory = view.findViewById<MaterialButton>(R.id.btnCancelCategory)
+        val btnSaveCategory = view.findViewById<MaterialButton>(R.id.btnSaveCategory)
+        val edtCategoryName = view.findViewById<TextInputEditText>(R.id.edtCategoryName)
+
 
         btnCancelCategory.setOnClickListener {
 
             findNavController().popBackStack()
+        }
+        btnSaveCategory.setOnClickListener {
+            val categoryName = edtCategoryName.text.toString().trim()
+            if (categoryName.isEmpty()) {
+
+                edtCategoryName.error = "Vui lòng nhập tên thể loại!"
+                edtCategoryName.requestFocus() // Đẩy con trỏ chuột nhấp nháy lại đúng ô đó
+            } else {
+                Toast.makeText(requireContext(), "Đã lưu thể loại: $categoryName", Toast.LENGTH_SHORT).show()
+                findNavController().popBackStack()
+            }
         }
     }
 }
