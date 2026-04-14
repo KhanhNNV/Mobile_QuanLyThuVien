1b245af4b468768c7bbfff39d71e48b89fdb698a
2Dwk <phamngochungduc@gmail.com>
2026-04-14 15:11:56 +0700

    cập nhật

--- mobileBE/src/main/java/com/uth/mobileBE/models/Library.java
+++ mobileBE/src/main/java/com/uth/mobileBE/models/Library.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 @Entity
-@Table(name = "librarys")
+@Table(name = "library")
 public class Library {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
